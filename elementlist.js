2eca8d1b350e20e92324df38d8ee0f2cf408966f
Alberto González <albertogonzalezcatalan@gmail.com>
2015-10-15 18:17:38 +0200

    Updated to v0.11

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","load()"],["f","loadPath()"],["c","Quaver\\Core\\Bootstrap"],["c","Quaver\\Core\\Config"],["c","Quaver\\Core\\Controller"],["c","Quaver\\Core\\DB"],["c","Quaver\\Core\\Exception"],["c","Quaver\\Core\\Helper"],["c","Quaver\\Core\\Lang"],["c","Quaver\\Core\\LangStrings"],["c","Quaver\\Core\\Log"],["c","Quaver\\Core\\Model"],["c","Quaver\\Core\\Resources"],["c","Quaver\\Core\\Router"]];
+ApiGen.elements = [["f","load()"],["f","loadPath()"],["c","Quaver\\Core\\App"],["c","Quaver\\Core\\Config"],["c","Quaver\\Core\\Controller"],["c","Quaver\\Core\\DB"],["c","Quaver\\Core\\Exception"],["c","Quaver\\Core\\Helper"],["c","Quaver\\Core\\Lang"],["c","Quaver\\Core\\LangStrings"],["c","Quaver\\Core\\Log"],["c","Quaver\\Core\\Model"],["c","Quaver\\Core\\Resources"],["c","Quaver\\Core\\Router"]];
